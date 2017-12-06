c63ba31b3102ac9c9905f8a3035dced4285215e2
Weslen Justino <weslen.justino@fatec.sp.gov.br>
2017-12-06 11:09:50 -0200

    imagem muito pequena

--- js/myjs.js
+++ js/myjs.js
@@ -21,20 +21,4 @@
 		}
 		document.getElementById(casasNome).style.display = "block";
 		evt.currentTarget.className += " active";
-	}
-	
-	<!-- BOTÃO TOP -->	
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			document.getElementById("myBtn").style.display = "block";
-		} else {
-			document.getElementById("myBtn").style.display = "none";
-		}
-	}
-	
-	function topFunction() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
+	}		
